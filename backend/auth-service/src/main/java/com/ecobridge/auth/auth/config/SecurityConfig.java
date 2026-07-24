d2526a385ecb31cc8a420c8d37c7fe4c0ca49f79
AASHUPA <thakurdivy84@gmail.com>
2026-07-24 08:18:22 +0530

    34'

--- backend/auth-service/src/main/java/com/ecobridge/auth/auth/config/SecurityConfig.java
+++ backend/auth-service/src/main/java/com/ecobridge/auth/auth/config/SecurityConfig.java
@@ -6,7 +6,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
@@ -113,22 +116,34 @@ public class SecurityConfig {
         return configuration.getAuthenticationManager();
     }
 //    @Bean
-//    public CorsConfigurationSource corsConfigurationSource(@Value("${app.cors.front-end-url}") String corsUrls) {
-//
-//        String[] urls = corsUrls.trim().split(",");
-//
-//        var config = new CorsConfiguration();
-//        config.setAllowedOrigins(Arrays.asList(urls));
-//        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD"));
-//        config.setAllowedHeaders(List.of("*"));
-//        config.setAllowCredentials(true);
-//
-//        var source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", config);
-//        return source;
-//
-//
-//    }
+public CorsConfigurationSource corsConfigurationSource() {
+
+    CorsConfiguration config = new CorsConfiguration();
+
+    config.setAllowedOrigins(List.of(
+            "http://localhost:5173",
+            "https://ebe-mauve.vercel.app"
+    ));
+
+    config.setAllowedMethods(List.of(
+            "GET",
+            "POST",
+            "PUT",
+            "DELETE",
+            "PATCH",
+            "OPTIONS"
+    ));
+
+    config.setAllowedHeaders(List.of("*"));
+    config.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source =
+            new UrlBasedCorsConfigurationSource();
+
+    source.registerCorsConfiguration("/**", config);
+
+    return source;
+}
 
 
 //to check actutor works :
