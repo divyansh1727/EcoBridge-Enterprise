7f5e6268c19c074a6a04de42ae20b204e29d83b5
AASHUPA <thakurdivy84@gmail.com>
2026-07-25 23:43:55 +0530

    fixed url error

--- frontend/ecobridge-frontend/src/services/analyticsService.js
+++ frontend/ecobridge-frontend/src/services/analyticsService.js
@@ -1,6 +1,6 @@
 import api from "../api/analyticsApi";
 
 export const getDashboardStats = async () => {
-  const response = await api.get("/api/v1/");
+  const response = await api.get("/api/v1/analytics/dashboard");
   return response.data;
 };
