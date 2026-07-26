fefd4359113399fe6d7e8d2e0ab5c25fabff3426
AASHUPA <thakurdivy84@gmail.com>
2026-07-26 14:26:01 +0530

    notif3

--- frontend/ecobridge-frontend/src/api/notificationApi.js
+++ frontend/ecobridge-frontend/src/api/notificationApi.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 
+console.log("Notification Base URL =", import.meta.env.VITE_NOTIFICATION_URL);
+
 const api = axios.create({
   baseURL: import.meta.env.VITE_NOTIFICATION_URL,
   withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
+  console.log("Final Request URL =", config.baseURL + config.url);
 
   const token = localStorage.getItem("token");
 
@@ -14,7 +17,6 @@ api.interceptors.request.use((config) => {
   }
 
   return config;
-
 });
 
 export default api;
